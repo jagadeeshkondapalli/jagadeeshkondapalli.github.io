a914a154ccabf54e07f5274674a4d1efb1d2b908
jagadeeshk <102029784+jagadeeshkondapalli@users.noreply.github.com>
2022-06-25 12:49:31 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+/*const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () =>{
     document.querySelector(".style-switcher").classList.toggle("open");
-})
+})*/
  window.addEventListener("scroll",()=>{
     if(document.querySelector(".style-switcher").classList.contains("open"))
     {
